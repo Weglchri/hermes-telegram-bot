ee0cdb5d158d100d687ebf305618c95aa933eec6
Weglchri <christopher.wegl@gmail.com>
2019-08-11 11:07:55 +0200

    added troll

--- index.js
+++ index.js
@@ -104,6 +104,11 @@ app.post('/', async (req, res) => {
           const textToSend = await quoter.askForQuote(sentMessage);
           sentMessages(req, res, textToSend); 
 
+     } else if (sentMessage.match(/rick/igm)) {
+          console.log(`${user} entered quote`);
+          const textToSend = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+          sentMessages(req, res, textToSend); 
+
      } else {
           console.log("Send response: 200");
           res.status(200).send({});

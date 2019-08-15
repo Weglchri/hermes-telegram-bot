c7c6aa82ace57ba2d9aca5814c7221a97423577a
Weglchri <christopher.wegl@gmail.com>
2019-08-15 18:02:39 +0200

    timezone offset removed

--- index.js
+++ index.js
@@ -50,8 +50,9 @@ app.use(bodyParser.json());
 app.post('/', async (req, res) => {
 
      console.log("Request Body: ", req.body);
-     console.log(quoter.getQuotesObject()["1"].metadata.date);
-     console.log(quoter.getQuotesObject()["1"].metadata.date.getTimezoneOffset());
+     var k = quoter.getQuotesObject()["1"].metadata.date
+     console.log(new Date(k));
+     console.log(new Date(k).getTimezoneOffset());
 
      var sentMessage = 'empty';
      var user = 'empty';
